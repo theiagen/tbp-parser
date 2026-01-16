d15fde5474649ba25741af6d4745eed796051ef6
Theron James <89421308+MrTheronJ@users.noreply.github.com>
2026-01-16 09:11:31 -0800

    v2.11.2 (#40)
    * Consider mutation QC when determining MTBC lineage ID
    
    * Update versioning, documentation, and Docker file

--- tbp_parser/LIMS.py
+++ tbp_parser/LIMS.py
@@ -60,8 +60,12 @@ class LIMS:
           self.logger.debug("LIMS:[tNGS only] The sequencing method is tNGS; now checking for a His57Asp mutation in pncA")
           pncA_mutations = globals_.DF_LABORATORIAN[(globals_.DF_LABORATORIAN["tbprofiler_gene_name"] == "pncA")]
           if "p.His57Asp" in pncA_mutations["tbprofiler_variant_substitution_aa"].tolist():
-            self.logger.debug("LIMS:[tNGS only] p.His57Asp detected in pncA, lineage is likely M. bovis")
-            lineage.add("DNA of Mycobacterium bovis detected")
+            if "Failed quality in the mutation position" in pncA_mutations["warning"].tolist():
+              self.logger.debug("LIMS:[tNGS only] p.His57Asp detected in pncA but failed quality in the mutation position; M. bovis cannot be ruled out")
+              lineage.add("DNA of Mycobacterium tuberculosis complex detected (M. bovis not ruled out)")
+            else:
+              self.logger.debug("LIMS:[tNGS only] p.His57Asp detected in pncA, lineage is likely M. bovis")
+              lineage.add("DNA of Mycobacterium bovis detected")
           else:
             self.logger.debug("LIMS:[tNGS only] p.His57Asp not detected in pncA, lineage is likely M. tuberculosis")
             lineage.add("DNA of Mycobacterium tuberculosis complex detected (not M. bovis)") 
