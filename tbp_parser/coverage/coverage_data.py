c4d45671b2931b52c58151e3d82f915d83600191
Theron James <theron.james@theiagen.com>
2026-02-10 03:55:21 +0000

    Update coverage_data.py module to use pydantic

--- tbp_parser/coverage/coverage_data.py
+++ tbp_parser/coverage/coverage_data.py
@@ -1,22 +1,14 @@
+from pydantic import BaseModel, computed_field
 
-class GeneCoverage:
+class GeneCoverage(BaseModel):
     """
       A class to represent coverage data for a single gene (BedRecord).
     """
-    def __init__(
-        self,
-        locus_tag: str,
-        gene_name: str,
-        coords: tuple[int, int],
-        breadth_of_coverage: float,
-        average_depth: float,
-    ) -> None:
-
-        self.locus_tag = locus_tag
-        self.gene_name = gene_name
-        self.coords = coords
-        self.breadth_of_coverage = breadth_of_coverage
-        self.average_depth = average_depth
+    locus_tag: str
+    gene_name: str
+    coords: tuple[int, int]
+    breadth_of_coverage: float
+    average_depth: float
 
     def __str__(self):
         return f"GeneCoverage([{self.gene_name}][{self.locus_tag}][{self.coords}] BC:{self.breadth_of_coverage} AD:{self.average_depth})"
@@ -24,45 +16,21 @@ class GeneCoverage:
     def __repr__(self):
         return f"GeneCoverage([{self.gene_name}][{self.locus_tag}][{self.coords}] BC:{self.breadth_of_coverage} AD:{self.average_depth})"
 
-    def __len__(self) -> int:
-        return self.length
-
-    @property
-    def length(self) -> int:
-        return self.coords[1] - self.coords[0] # already 0-based from BedRecord
-
-
-class LocusCoverage:
+class LocusCoverage(BaseModel):
     """
     A class to represent coverage data for a single locus tag, aggregating multiple regions if necessary.
     """
-    def __init__(
-        self,
-        locus_tag: str,
-        gene_names: list[str],
-        coords: list[tuple[int, int]], # can be a list of coordinates if aggregating multiple regions
-        breadth_of_coverage: float,
-        average_depth: float,
-    ) -> None:
-
-        self.locus_tag = locus_tag
-        self.gene_names = gene_names
-        self.coords = coords
-        self.breadth_of_coverage = breadth_of_coverage
-        self.average_depth = average_depth
+    locus_tag: str
+    gene_names: list[str]
+    coords: list[tuple[int, int]] # can be a list of coordinates if aggregating multiple regions
+    breadth_of_coverage: float
+    average_depth: float
 
     def __str__(self):
         return f"LocusCoverage([{self.gene_names}][{self.locus_tag}][{self.coords}] BC:{self.breadth_of_coverage} AD:{self.average_depth})"
 
     def __repr__(self):
         return f"LocusCoverage([{self.gene_names}][{self.locus_tag}][{self.coords}] BC:{self.breadth_of_coverage} AD:{self.average_depth})"
-
-    def __len__(self) -> int:
-        return self.length
-
-    @property
-    def length(self) -> int:
-        return sum(end - start for start, end in self.coords)
 
     def contains_position(self, position: int) -> bool:
         """Check if a specific position exists within locus coverage.
