716e122871f73b0bad89f29808c117ac6b02865a
Theron James <theron.james@theiagen.com>
2026-02-11 22:31:29 +0000

    Make arg with underscores

--- tbp_parser/arguments.py
+++ tbp_parser/arguments.py
@@ -70,7 +70,7 @@ def parse_arguments():
     qc_arguments.add_argument("-l", "--min_percent_loci_covered", default=0.7, metavar="\b", type=float,
                         help="the minimum percentage of loci/genes in the LIMS report that must pass coverage QC for the sample to be identified as MTBC (0.7 -> 70%%)\ndefault=0.7")
 
-    qc_arguments.add_argument("--qc-resistant-mutations", default=False, action="store_true",
+    qc_arguments.add_argument("-q", "--qc_resistant_mutations", default=False, action="store_true",
                         help="treat R mutations the same as S or U mutations in that if locus coverage is poor, they will not be reported\ndefault=False")
 
     general_arguments = parser.add_argument_group("text arguments",
