79cf6316a158090d454f1377ea6c18a5a9b9bb22
Theron James <theron.james@theiagen.com>
2026-02-26 02:15:59 +0000

    Add overlap_resolution pytest
    Manually confirmed the math for this example and tried to leave an
    explanation as it gets quite messy

--- tests/test_coverage_calculator.py
+++ tests/test_coverage_calculator.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import pytest
 
 class TestPopulateReadsByPosition:
     def test_populate_reads_by_position(self, make_cov_calc, make_bed_record):
@@ -155,37 +156,49 @@ class TestGenerateCoverageMaps:
         assert locus_map["Rv0000"].average_depth == 20.0
 
 class TestComplexOverlappingRecords:
-    def test_complex_overlapping_records(self, make_cov_calc, make_bed_record):
+    @pytest.fixture
+    def setup(self, make_cov_calc, make_bed_record):
         calc = make_cov_calc(cov_start=0, cov_end=100, read_length=10)
         calc.config.MIN_DEPTH = 8
-        self.config = calc.config
-
+        calc.config.TNGS = True
         record1 = make_bed_record(1, 40, locus_tag="Rv0000", gene_name="geneA")
         record2 = make_bed_record(25, 75, locus_tag="Rv0000", gene_name="geneB")
         record3 = make_bed_record(55, 100, locus_tag="Rv0000", gene_name="geneC")
-        all_records = [record1, record2, record3]
+        calc.populate_reads_by_position([record1, record2, record3])
+        return calc, record1, record2, record3
 
-        calc.populate_reads_by_position(all_records)
-
-        # before resolving overlaps confirm that all records overlap (if applicable)
+    def test_overlaps_detected(self, setup):
+        calc, record1, record2, record3 = setup
         assert record1.overlaps_with(record2) == True
         assert record2.overlaps_with(record1) == True
         assert record2.overlaps_with(record3) == True
         assert record3.overlaps_with(record2) == True
         assert record1.overlaps_with(record3) == False
 
-        # before resolving overlaps, the non-overlapping coords should be as follows:
+    def test_non_overlapping_coords(self, setup):
+        calc, record1, record2, record3 = setup
         assert record1.get_non_overlapping_coords([record2, record3]) == [(1, 24)]
         assert record2.get_non_overlapping_coords([record1, record3]) == [(41, 54)]
         assert record3.get_non_overlapping_coords([record1, record2]) == [(76, 100)]
 
-        # Coverage increases linearly from 1 to 10 read-pairs across positions 1–10,
-        # giving an average depth of 11 and 0.7 breadth (positions 1–3 < 8 reads).
-        first_10_positions = {k: v for k, v in record1.reads_by_position.items() if k <= 10}
-        assert calc._calculate_breadth_of_coverage(first_10_positions) == 0.7
-        assert calc._calculate_average_depth(first_10_positions) == 11.0
+    def test_overlapping_coords(self, setup):
+        calc, record1, record2, record3 = setup
+        assert record1.overlapping_coords(record2) == (25, 40)
+        assert record2.overlapping_coords(record1) == (25, 40)
+        assert record2.overlapping_coords(record3) == (55, 75)
+        assert record3.overlapping_coords(record2) == (55, 75)
 
-        # before resolving overlaps, the reads in the overlapping regions should be as follows:
+    def test_first_9_positions_coverage(self, setup):
+        calc, record1, record2, record3 = setup
+
+        # Coverage increases linearly from 1 to 9 read-pairs across positions 1–9,
+        # giving an average depth of 10 and 0.666667 breadth (positions 1–3 < 8 reads).
+        first_9_positions = {k: v for k, v in record1.reads_by_position.items() if k <= 9}
+        assert calc._calculate_breadth_of_coverage(first_9_positions) == float(6/9)
+        assert calc._calculate_average_depth(first_9_positions) == 10.0
+
+    def test_overlap_inflated_coverage(self, setup):
+        calc, record1, record2, record3 = setup
         overlap_coords = [
             record1.overlapping_coords(record2),
             record2.overlapping_coords(record1),
@@ -216,27 +229,66 @@ class TestComplexOverlappingRecords:
         assert calc._calculate_breadth_of_coverage(all_overlap_reads_by_position) == 1.0
         assert calc._calculate_average_depth(all_overlap_reads_by_position) == 40.0
 
-        # before resolving overlaps, the breadth of coverage and average depth should be as follows:
-        target_map, locus_map = calc.generate_coverage_maps(all_records)
+    def test_coverage_no_overlap_resolution(self, setup):
+        calc, record1, record2, record3 = setup
+        calc.config.TNGS = False # not resolving overlaps, so breadth and average depth will be inflated in the overlapping regions
+        target_map, locus_map = calc.calculate([record1, record2, record3])
 
+        # before resolving overlaps, the breadth of coverage and average depth should be as follows:
         # BedRecord([geneA][Rv0000](1, 40))
         assert target_map["geneA"].breadth_of_coverage == 0.925 # 37/40 >= min depth
         assert target_map["geneA"].average_depth == 17.75 # 710/40
-
         # BedRecord([geneB][Rv0000](25, 75))
         assert target_map["geneB"].breadth_of_coverage == 1.0 # 51/51 >= min depth
         assert target_map["geneB"].average_depth == 20.0 # 1020/51
-
         # BedRecord([geneC][Rv0000](55, 100))
         assert target_map["geneC"].breadth_of_coverage == 1.0 # 46/46 >= min depth
         assert target_map["geneC"].average_depth == 20.0 # 920/46
 
         # AVG DEPTH ACROSS LOCUS:
-        # [1-10]: 11 (110 reads) (10 pos)
-        # [11-24]: 20 (280 reads) (14 pos)
+        # [1-9]: 10 (90 reads) (9 pos)
+        # [10-24]: 20 (300 reads) (15 pos)
         # [25-40]: 20*2 (320*2 reads) (16 pos)
         # [41-54]: 20 (280 reads) (14 pos)
         # [55-75]: 20*2 (420*2 reads) (21 pos)
         # [76-100]: 20 (500 reads) (25 pos)
         assert locus_map["Rv0000"].breadth_of_coverage == 0.97 # 97/100 >= min depth
         assert locus_map["Rv0000"].average_depth == 26.50 # 2650/100
+
+    def test_coverage_with_overlap_resolution(self, setup):
+        calc, record1, record2, record3 = setup
+        calc.config.TNGS = True # resolving overlaps
+        target_map, locus_map = calc.calculate([record1, record2, record3])
+        # Reminder of non overlapping regions
+        # [(1, 24)] , [(41, 54)] , [(76, 100)]
+
+        # after resolving overlaps, the breadth of coverage and average depth should be as follows:
+        # Ex) reads starting at pos 24 (last non-overlapping pos) extend and taper off through pos 33 (24 + 10 (read_length) - 1)
+        # record1 coverage: [1-3] below min, [4-24]: above min depth, [25-30]: above min depth, [31-33]: below min depth, [34-40]: below min depth
+
+        # BedRecord([geneA][Rv0000](1, 40))
+        assert target_map["geneA"].breadth_of_coverage == 0.675 # 27/40 [4-30] >= min depth
+        assert target_map["geneA"].average_depth == 12.0 # 480/40 [1-9] (90 reads), [10-24]: (300 reads), [25-33] (90 reads), [34-40] (0 reads)
+
+        # BedRecord([geneB][Rv0000](25, 75))
+        assert target_map["geneB"].breadth_of_coverage == float(26/51) # 26/51 pos [35-60] >= min depth
+        assert target_map["geneB"].average_depth == float(460/51) # 460/51 [25-31] (0 reads), [32-40] (90 reads), [41-54] (280 reads), [55-63] (90 reads), [64-75] (0 reads)
+
+        # BedRecord([geneC][Rv0000](55, 100))
+        assert target_map["geneC"].breadth_of_coverage == float(31/46) # 31/46 pos [70-100] >= min depth
+        assert target_map["geneC"].average_depth == float(590/46) # 590/46 [55-66] (0 reads), [67-75] (90 reads), [76-100] (500 reads)
+
+        # AVG DEPTH ACROSS LOCUS:
+        # [1-9]: 10 (90 reads)
+        # [10-24]: 20 (300 reads)
+        # [25-31]: (84 reads *record1)
+        # [32]: (4 reads *record1 + 2 reads *record2)
+        # [33]: (2 reads *record1 + 4 reads *record2)
+        # [34-40]: (84 reads *record2)
+        # [41-54] (280 reads)
+        # [55-63] (90 reads *record3)
+        # [64-66] (0 reads)
+        # [67-75] (90 reads)
+        # [76-100] (500 reads)
+        assert locus_map["Rv0000"].breadth_of_coverage == 0.84 # 84/100 below min_depth [4-30], [35-60], [70-100]
+        assert locus_map["Rv0000"].average_depth == 15.30 # 1530/100 [480 + 460 + 590]
