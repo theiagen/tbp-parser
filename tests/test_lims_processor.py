18a1125c2b7b8a0dc0726687cb666660b2b9825a
Sage Wright <sage.wright@theiagen.com>
2026-04-27 17:27:55 +0000

    sorry for all the emails theron

--- tests/test_lims_processor.py
+++ tests/test_lims_processor.py
@@ -401,7 +401,7 @@ class TestResolveDrugTarget:
         record.gene_codes["rpoB"].max_mdl_variants = [v]
         processor.resolve_drug_target(record)
         # assert record.drug_target_value == "No mutations associated with resistance to rifampicin detected"
-        # updated in v3.0.3 - NA (operationally WT) rpoB LIMS has same message as S
+        # updated in v3.0.3 - WT rpoB LIMS has same message as S
         assert record.drug_target_value == "Predicted susceptibility to rifampicin"
         
     # def test_rpob_na_returns_no_mutations(self, processor, make_lims_record, make_variant):        
